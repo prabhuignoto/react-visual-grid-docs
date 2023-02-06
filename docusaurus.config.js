2a3cffd43190276e008d4b2c48bda4bfaeb2716c
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2023-02-06 11:55:32 +0530

    enabling algolia search configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,59 +1,59 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'React Visual Grid',
-  tagline: 'Build beautiful and powerful image grids',
-  url: 'https://react-visual-grid.prabhumurthy.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "React Visual Grid",
+  tagline: "Build beautiful and powerful image grids",
+  url: "https://react-visual-grid.prabhumurthy.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'prabhuignoto', // Usually your GitHub org/user name.
-  projectName: 'react-visual-grid', // Usually your repo name.
+  organizationName: "prabhuignoto", // Usually your GitHub org/user name.
+  projectName: "react-visual-grid", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/prabhuignoto/react-visual-grid-docs/tree/main/',
+            "https://github.com/prabhuignoto/react-visual-grid-docs/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/prabhuignoto/react-visual-grid-docs/tree/main/',
+            "https://github.com/prabhuignoto/react-visual-grid-docs/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         sitemap: {
-          changefreq: 'weekly',
+          changefreq: "weekly",
           priority: 0.5,
-          ignorePatterns: ['/tags/**'],
-          filename: 'sitemap.xml',
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
         },
       }),
     ],
@@ -62,11 +62,40 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "LSRH7VVCXG",
+
+        // Public API key: it is safe to commit it
+        apiKey: "0b9ff1f4cadc4212399ac0da931bfd58",
+
+        indexName: "react-visual-grid-prabhumurthy",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: "/docs/", // or as RegExp: /\/docs\//
+        //   to: "/",
+        // },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
       navbar: {
-        title: 'react-visual-grid',
+        title: "react-visual-grid",
         logo: {
-          alt: 'react-visual-grid Logo',
-          src: 'img/logo.svg',
+          alt: "react-visual-grid Logo",
+          src: "img/logo.svg",
         },
         items: [
           // {
@@ -77,30 +106,30 @@ const config = {
           // },
           // {to: '/blog', label: 'Github', position: 'left'},
           {
-            href: 'https://github.com/prabhuignoto/react-visual-grid',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/prabhuignoto/react-visual-grid",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Getting Started',
-                to: 'docs/getting-started/introduction',
+                label: "Getting Started",
+                to: "docs/getting-started/introduction",
               },
               {
-                label: 'Properties',
-                to: 'docs/props',
+                label: "Properties",
+                to: "docs/props",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               // {
               //   label: 'Stack Overflow',
@@ -111,21 +140,21 @@ const config = {
               //   href: 'https://discordapp.com/invite/docusaurus',
               // },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/prabhumurthy2',
+                label: "Twitter",
+                href: "https://twitter.com/prabhumurthy2",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               // {
               //   label: 'Blog',
               //   to: '/blog',
               // },
               {
-                label: 'GitHub',
-                href: 'https://github.com/prabhuignoto/react-visual-grid',
+                label: "GitHub",
+                href: "https://github.com/prabhuignoto/react-visual-grid",
               },
             ],
           },
